a3778e12057f4fbb83f642ae54e7a093a691e7df
John Doe <79935610+NfolZordo@users.noreply.github.com>
2022-11-10 09:39:09 +0200

    q

--- L2/wiki/encyclopedia/urls.py
+++ L2/wiki/encyclopedia/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("wiki", views.index, name="index"),
     path('wiki/random', views.randomArticle, name="random"),
     path('wiki/creation', views.creation, name="creation"),
